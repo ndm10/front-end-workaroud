cd2e60e39cb9b6da923baa21c23e6e1936b9d78e
Minh Nguyen <ndminh1010@gmail.com>
2026-06-02 10:16:19 +0700

    Study array basic method

--- assets/js/js-study.js
+++ assets/js/js-study.js
@@ -8,11 +8,12 @@ console.log(...arr);
 console.log(fruits instanceof Array);
 console.log(Array.isArray(fruits))
 
-/* =================Array method test====================== */
+/* =================Array basic method test====================== */
 console.log('Array length:', arr.length)
 arr.length = 7;
 console.log('Array length after change:', arr.length)
 console.log(arr)
+
 console.log(arr.at(-1));
 // console.log(arr[-1]); undefined
 console.log('element return after using pop():', arr.pop())
@@ -24,10 +25,12 @@ console.log('Array after is shifted:', arr)
 console.log(arr.unshift("Unshift"))
 console.log('Array after is unshifted:', arr)
 console.log('Array using join():', arr.join(' . '))
+
 arr[0] = "Hello"
 console.log('After Change the first element', arr)
 console.log('Is \`arr\` an array', Array.isArray(arr))
 console.log('Is 1 an array', Array.isArray(1))
+
 const str1 = "Hello"
 const str2 = "word"
 const arr2 = [49, 'new arr2', 'hello', 22, 423]
@@ -36,6 +39,25 @@ console.log('str concat', str1.concat(str2))
 console.log('Arr with concatenating', arr.concat(arr2))
 console.log('Arr with multiple concatenating', arr.concat(arr2, arr3))
 console.log('Arr with string concatenating', arr.concat('string1', 'string 2'))
+
 arr2.copyWithin(2, 0, 2)
 console.log('Array2 after using copyWithin():', arr2)
-/* =================Array method test====================== */
+
+const arrTwoDimension = [[12, 13], [14, 15], [16, 17]]
+console.log('Array before using flat:', arrTwoDimension);
+console.log('Array after using flat:', arrTwoDimension.flat());
+const arrFlatMap = [12, 13, 14, 15, 16, 17]
+console.log('Array before using flat map:', arrFlatMap);
+console.log('Array after using flat map:', arrFlatMap.flatMap(x => [x, x * 10, x * 100]));
+
+
+const spliceArr = [12,32,52]
+console.log('Array before splice:', spliceArr);
+spliceArr.splice(1, 5, 'hello', 'Minh')
+console.log('Array after splice:', spliceArr)
+
+const arrBeforeSlice = [34,25,12,32,52]
+const arrAfterSlice = arrBeforeSlice.slice(3)
+console.log('Original array:', arrBeforeSlice)
+console.log('Sliced array:', arrAfterSlice)
+/* =================Array basic method test====================== */
